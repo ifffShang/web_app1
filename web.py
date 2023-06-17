d6805029110430267d3098cfb2aa06270f19392c
15531 <2906161529@qq.com>
2023-06-17 17:16:29 +0800

    Initial commit

---
+++ web.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import function
+def add_todo():
+    todo = st.session_state['new_todo'] + "\n"
+    todos.append(todo)
+    print(todo)
+    function.write_todos(todos)
+
+todos = function.get_todos()
+
+st.title("My To-do App")
+st.subheader('This is my to-do wwapp.')
+st.write('This app is my to-do app.')
+for index, choice in enumerate(todos):
+    checkbox = st.checkbox(choice,key= choice)
+    if checkbox:
+        todos.pop(index)
+        function.write_todos(todos)
+        del st.session_state[choice]
+        st.experimental_rerun()
+
+st.text_input(label="Enter a todo: ", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
+
